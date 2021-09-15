0e17080404940212c31d8b22efa783a2c3907e3f
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-14 20:47:24 -0700

    Bumped istio module version

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ module "elastic" {
 
 module "istio" {
   source  = "app.terraform.io/coffeeshopmicroservicedemo/istio/helm"
-  version = "0.1.0-alpha.2"
+  version = "0.1.0-alpha.3"
 
   do_project_id = module.kubernetes.do_project_id
   do_token = var.do_token
