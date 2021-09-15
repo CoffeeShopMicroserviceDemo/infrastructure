0b1827c0781905e8e6641fef5276e26b4d5a1395
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-15 11:19:48 -0700

    Temporarily removed helm dependencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,29 +27,29 @@ module "kubernetes" {
   do_token = var.do_token
 }
 
-module "elastic" {
-  source  = "app.terraform.io/coffeeshopmicroservicedemo/elastic/helm"
-  version = "0.1.0-alpha.2"
-
-  do_project_id = module.kubernetes.do_project_id
-  do_k8s_name = module.kubernetes.k8s_cluster_name
-  do_token = var.do_token
-}
-
-module "istio" {
-  source  = "app.terraform.io/coffeeshopmicroservicedemo/istio/helm"
-  version = "0.1.0-alpha.4"
-
-  do_project_id = module.kubernetes.do_project_id
-  do_k8s_name = module.kubernetes.k8s_cluster_name
-  do_token = var.do_token
-}
-
-module "kubesphere" {
-  source  = "app.terraform.io/coffeeshopmicroservicedemo/kubesphere/helm"
-  version = "0.1.0-alpha.3"
-
-  do_project_id = module.kubernetes.do_project_id
-  do_k8s_name = module.kubernetes.k8s_cluster_name
-  do_token = var.do_token
-}
+#module "elastic" {
+#  source  = "app.terraform.io/coffeeshopmicroservicedemo/elastic/helm"
+#  version = "0.1.0-alpha.2"
+#
+#  do_project_id = module.kubernetes.do_project_id
+#  do_k8s_name = module.kubernetes.k8s_cluster_name
+#  do_token = var.do_token
+#}
+#
+#module "istio" {
+#  source  = "app.terraform.io/coffeeshopmicroservicedemo/istio/helm"
+#  version = "0.1.0-alpha.4"
+#
+#  do_project_id = module.kubernetes.do_project_id
+#  do_k8s_name = module.kubernetes.k8s_cluster_name
+#  do_token = var.do_token
+#}
+#
+#module "kubesphere" {
+#  source  = "app.terraform.io/coffeeshopmicroservicedemo/kubesphere/helm"
+#  version = "0.1.0-alpha.3"
+#
+#  do_project_id = module.kubernetes.do_project_id
+#  do_k8s_name = module.kubernetes.k8s_cluster_name
+#  do_token = var.do_token
+#}
